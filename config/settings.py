83b6decd74eef11016f49a9454cd906464343376
tetsuya <satoh1529@gmail.com>
2021-07-17 00:45:29 +0900

    試し

--- config/settings.py
+++ config/settings.py
@@ -202,9 +202,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'dj_rest_auth.utils.JWTCookieAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ],
 }
 
