0ea186753d8dd09148c2f78f76aa71f55728169d
tetsuya <satoh1529@gmail.com>
2021-07-17 09:29:07 +0900

    試し

--- config/settings.py
+++ config/settings.py
@@ -198,7 +198,7 @@ SIMPLE_JWT = {
 # drf設定
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'dj_rest_auth.utils.JWTCookieAuthentication',
