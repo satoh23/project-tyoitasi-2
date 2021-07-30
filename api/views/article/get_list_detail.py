f2dc9105a148040643e8e9ae4bde01c6b6e1732a
tetsuya <satoh1529@gmail.com>
2021-07-30 14:27:20 +0900

    フィルターの追加

--- api/views/article/get_list_detail.py
+++ api/views/article/get_list_detail.py
@@ -1,11 +1,21 @@
 from rest_framework.permissions import AllowAny
 from rest_framework import generics
+from django_filters import rest_framework as filters
 from article.serializers import ListDetailSerializer
 from article.models import Detail
+
+
+class ArticleFilter(filters.FilterSet):
+    main_material = filters.CharFilter(
+        name="main_material", lookup_expr='contains')
+
+    class Meta:
+        model = Detail
+        fields = ['main_material', ]
 
 
 class ListDetailView(generics.ListAPIView):
     queryset = Detail.objects.all()
     serializer_class = ListDetailSerializer
     permission_classes = (AllowAny,)
-    filterset_fields = ['main_material', ]
+    filter_class = ArticleFilter
